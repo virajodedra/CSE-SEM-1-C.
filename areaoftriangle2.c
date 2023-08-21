eae03abd9cfff21422afed6fce043380cb1241a9
virajodedra <141026170+virajodedra@users.noreply.github.com>
2023-08-21 16:46:21 +0530

    Add files via upload

---
+++ areaoftriangle2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int  main() {
+  	int h,l,area;
+  	printf("fill the value of h :");
+  	scanf("%d",&h);
+  	printf("fill the value of l :");
+  	scanf("%d",&l);
+  	 
+  	 area=(h*l)/2;
+  	 printf("the area is :%d",area);
+  }
