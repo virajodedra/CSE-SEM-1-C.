d57a4295fbafe68b3c72701068dbe00a72ce4be0
virajodedra <141026170+virajodedra@users.noreply.github.com>
2023-08-22 15:13:36 +0530

    Add files via upload

---
+++ greaterfrom2numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+   void main() {
+   	float a,b;
+   	printf("enter the number A :");
+   	scanf("%f",&a);
+   	
+   	printf("enter the number B :");
+   	scanf("%f",&b);
+   	
+   	if(a>b){
+   		printf("A is greater");
+   		
+	   }else{
+	   	printf("B is greater");
+	   }
+   	
+   }
+
