026309fdac026a11b307a241cdad440efa7bc2bc
virajodedra <141026170+virajodedra@users.noreply.github.com>
2023-08-22 11:09:42 +0530

    Add files via upload

---
+++ fernhit.c
@@ -0,0 +1,12 @@
+//fernhit to celsus
+#include<stdio.h>
+void main()
+{
+	float f,c;
+	printf("enter the value of ferhit =");
+	scanf("%f",&f);
+	c=(((f-32)*5)/9);
+	printf("the celsus is %f",c);
+	
+}
+
