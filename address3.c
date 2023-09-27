6ec6d04ca28f27b9fa907aefab61979501f03e5b
Odedra Viraj Arjanbhai <141026170+virajodedra@users.noreply.github.com>
2023-09-27 09:12:15 +0530

    Delete address3.c

--- address3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main(){
-	printf("\'street no.123'\n");
-	printf("home name:dill banglloe\n");
-	printf("society name:hari aum society\n");
-	printf("city:kutiyana,pincode:362650");
-}
