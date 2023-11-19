6ece4fb14dec63da82c4d0a52e09d34f66bdfb6f
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2023-11-19 20:04:04 +0530

    Add files via upload

---
+++ 13-B-E.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+   void main(){
+   	
+   	int n=5,i,j;
+   	
+   	for(i=1;i<=n;i++){
+   		
+   		   for(j=1;j<=n;j++){
+   		   	
+   		   	
+   		   	if(i==1 ||i==5 || j==1 || j==5){
+   		   		
+   		   		printf("*");
+				  }
+				  else {
+				  	
+				 printf(" ");
+				  }
+				  
+				  
+			  }
+			  
+			  printf("\n");
+	   }
+   	
+   	
+   }
